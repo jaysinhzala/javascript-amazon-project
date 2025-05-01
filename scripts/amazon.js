adc13b1850420eb626a55cc4464e4a5ac49567a8
jaysinhzala1 <jaysinhzala1@gmail.com>
2025-05-01 14:53:01 -0400

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,10 @@
-//array object to save product info
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 86
-    },
-    priceCents: 1090
-},{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
 
+//Products array is created in another file named products.js
 //ForEach loop to add html code for every product in page
 //Add every product html code to productHTML string variable
 /* On line 56 - The toFixed() method converts a number to a string.
    The toFixed() method rounds the string to a specified number of decimals */
+
 let productHTML = '';
 products.forEach((product) => {
     productHTML += `
