a0c15795f1df9b44468a334449df995727fbc925
jaysinhzala1 <jaysinhzala1@gmail.com>
2025-05-04 16:06:45 -0400

    Creat automated tests for formate currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,28 @@
+//This file is for test different test cases for Money.js function
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('Test Suits : FormatCurrency');
+//Below line is name of test cases
+console.log('Convert Cents into Doller');
+if (formatCurrency(2095) === '20.95') {
+    console.log('Passed');
+}
+else {
+    console.log('Failed');
+}
+
+console.log('Works with 0');
+if (formatCurrency(0) === '0.00') {
+    console.log('Passed');
+}
+else {
+    console.log('Failed');
+}
+
+console.log('Rounds up for nearest cents');
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('Passed');
+}
+else {
+    console.log('Failed');
+}
